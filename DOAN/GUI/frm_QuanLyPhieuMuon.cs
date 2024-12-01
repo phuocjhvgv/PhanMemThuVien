f1820f4c5f9096ff20642afb8d251ae350121fd9
phuocjhvgv <phuochpv999@gmail.com>
2024-12-01 19:51:30 +0700

    Cập nhật chức năng quản lý sách và phiếu mượn

---
+++ DOAN/GUI/frm_QuanLyPhieuMuon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DOAN.GUI
+{
+    public partial class frm_QuanLyPhieuMuon : Form
+    {
+        public frm_QuanLyPhieuMuon()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_TimKiem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
